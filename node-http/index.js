36214bd7888594c2cd637b8c5855449eb60fcdcf
narahda <limnarahda@gmail.com>
2021-09-25 13:21:27 +0800

    added morgan
    for serving static files??

--- node-http/index.js
+++ node-http/index.js
@@ -44,5 +44,5 @@ const server = http.createServer((req, res) => {
   })
 
 server.listen(port, hostname, () => {
-    console.log('server running at http://${hostname}:${port}/');
+    console.log(`server running at http://${hostname}:${port}/`);
 });
