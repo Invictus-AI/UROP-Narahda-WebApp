d68115271d7265fe5417f3e7c7bdadb70430de25
narahda <limnarahda@gmail.com>
2021-11-08 23:34:04 +0800

    removed items
    remove to update files

--- server/app/database/db.js
+++
@@ -1,8 +0,0 @@
-const Pool = require("pg").Pool;
-
-const pool = new Pool();
-
-module.exports = {
-    query: (text, params) => pool.query(text,params),
-
-};
--- server/camera/cleaner.js
+++
@@ -1,6 +0,0 @@
-const findRemoveSync = require('find-remove')
-
-setInterval(() => {
-    var result = findRemoveSync('./camera/videos/ipcam', { age: { seconds: 30 }, extensions: '.ts' });
-    console.log(result);
-}, 5000);
